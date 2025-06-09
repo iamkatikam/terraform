9a08d90be05dc61ad481d62e5e2741b0764c1c3f
Ramesh Katikam <katikam.rams@gmail.com>
2025-06-09 19:18:42 +0530

    terraform ec2 creation

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,3 +1,12 @@
+resource "aws_instance" "web" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
   description = "Allow TLS inbound traffic and all outbound traffic"
