74913e2d2a30e5845c83d83125801b98bec9ffba
Ramesh Katikam <katikam.rams@gmail.com>
2025-06-20 19:12:30 +0530

    ec2 module changes

---
+++ vpc-module-test/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.99.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "roboshop-cart-remote-state"
+    key = "vpc-test"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
