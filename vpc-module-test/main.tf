c53d408c1da841cd1ab36a35d6e94f5e145f8543
Ramesh Katikam <katikam.rams@gmail.com>
2025-06-22 00:41:04 +0530

    vpc module changes

--- vpc-module-test/main.tf
+++ vpc-module-test/main.tf
@@ -1,8 +1,9 @@
 module "vpc" {
-  source              = "../terraform-aws-vpc"
-  project             = var.project
-  environment         = var.environment
-  public_subnet_cidrs = var.public_subnet_cidrs
-  private_subnet_cidrs = var.private_subnet_cidrs
+  source                = "../terraform-aws-vpc"
+  project               = var.project
+  environment           = var.environment
+  public_subnet_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
   database_subnet_cidrs = var.database_subnet_cidrs
+  is_peering_required = true
 }
--- vpc-module-test/variables.tf
+++ vpc-module-test/variables.tf
@@ -1,29 +1,29 @@
 variable "project" {
-    default = "roboshop"
-    type = string
-  
+  default = "roboshop"
+  type    = string
+
 }
 
 variable "environment" {
-    default = "dev"
-    type = string
-  
+  default = "dev"
+  type    = string
+
 }
 
 variable "public_subnet_cidrs" {
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
-    type = list(string)
-  
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  type    = list(string)
+
 }
 
 variable "private_subnet_cidrs" {
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
-    type = list(string)
-  
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+  type    = list(string)
+
 }
 
 variable "database_subnet_cidrs" {
-    default = ["10.0.21.0/24", "10.0.22.0/24"]
-    type = list(string)
-  
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
+  type    = list(string)
+
 }
