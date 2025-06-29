77b7e9854002533b8c31268e6340718bd69d89f7
Ramesh Katikam <katikam.rams@gmail.com>
2025-06-29 15:41:42 +0530

    provisioners in terraform

--- state/ec2.tf
+++ state/ec2.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "allow_all" {
   name        = var.security_group_name
   description = var.security_group_description
 
-  egress {
+  ingress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
